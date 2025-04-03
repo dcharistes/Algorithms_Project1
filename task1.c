878b27daea902d315ba524243852ec3656f7fee4
Karatsiolis <konstantinos.karatsiolis@gmail.com>
2025-04-03 14:36:51 +0300

    Task1

--- task1.c
+++ task1.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int arithm_sequence2(int a, int d, int n);
 
 int main(){
-	printf("----");
+    int a, d, n;
+
+    printf("Enter the first number of arithmetic progression (a): \n");
+    scanf("%d", &a);
+    printf("Enter the common difference (d): \n");
+    scanf("%d", &d);
+    printf("Enter the term number (n): \n");
+    scanf("%d", &n);
+
+    //printf("The %dth term of the sequence using the recursion is: %d\n", n, arithm_sequence1(n, a, d));
+    printf("The %dth term of the sequence using the non-recursion is: %d\n", n, arithm_sequence2( a, d, n));
+
+    return 0; 
 }
 
+//recursive
+/*int arithm_sequence1(int a, int d, int n){
+    if (n == 1) {
+        return a;
+    } else {
+        return d + arithm_sequence1(n - 1, a, d);
+    }
+}*/
+
+
+//non-recursive
+int arithm_sequence2(int a, int d, int n){
+    return a + (n - 1) * d;
+}
