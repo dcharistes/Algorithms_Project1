86a9647cd0f4face91f23ccd63bb68099f45ba3e
dcharistes <d.charistes@gmail.com>
2025-04-14 20:25:31 +0300

    corrected everything

--- task3.c
+++ task3.c
@@ -22,7 +22,7 @@ int main() {
 
     srand(time(NULL));  // Αρχικοποίηση seed για τυχαίους αριθμούς
     
-    int sizes[] = {6000, 7000, 8000, 9000, 10000};
+    int sizes[] = {60000, 70000, 80000, 90000, 100000};
     int num_sizes = sizeof(sizes) / sizeof(sizes[0]);
     int num_tests = 10;
     int dmin_value = INT_MAX;
